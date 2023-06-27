fe3222ec4c5aa62fb99fd9e982e2cc94d5833874
kn4ts <1110natsuki@gmail.com>
2023-06-27 13:54:24 +0900

    update code

--- mavlink_with_esp32/mavlink_with_esp32.ino
+++ mavlink_with_esp32/mavlink_with_esp32.ino
@@ -11,34 +11,65 @@ void setup() {
 
   setupTimer();	// タイマー設定用の関数
 
-  Serial2.begin(57600);      // シリアル通信（to Pixhawk6c）を開始
-  // Serial2.begin(115200);      // シリアル通信（to Pixhawk6c）を開始
+  // Serial2.begin(57600);      // シリアル通信（to Pixhawk6c）を開始
+  Serial2.begin(115200);      // シリアル通信（to Pixhawk6c）を開始
 }
 
 // Loop関数
 void loop() {
+  // HeartBeat の送信
+  SendHeartBeat();
+  // データストリーム の要求
+  // Mav_Request_Data();
 
-  // タイマー割り込みフラグが上がっていたら
-  if (Flag_timer > 0 ){
-    // Heartbeatの送信
-    SendHeartBeat();
+  // 計測値格納用変数の定義
+  uint32_t ctl;
+  float roll;
+  float pitch;
+  float yaw;
+  float rollspeed;
+  float pitchspeed;
+  float yawspeed;
 
-    SendCmdServo( 1, 1500);
-//    SendCmdServo( 1, 2000);
+  // メインループ
+  while(1){
+    // タイマー割り込みフラグの判定
+    if (Flag_timer > 0 ){
+      // Heartbeat信号の要求（?）
+      // SendHeartBeat();
 
+      // PWM値の計算
+      float outputfloat = 1000+(roll+PI)*1000/(2*PI) ;
+      //Serial.println(roll);
 
-    // MAV data の要求
-    num_hbs_pasados++;
-    if(num_hbs_pasados>=num_hbs) {
-      // Pixhawk からの stream を要求する
-      Mav_Request_Data();
-      num_hbs_pasados=0;
+      // PWM出力
+      // SendCmdServo( 1, 1500);
+      SendCmdServo( 1, (int)outputfloat);
+      SendCmdServo( 2, (int)outputfloat);
+      // SendCmdServo( 3, (int)outputfloat);
+      // comm_receive();
+      // SendCmdServo( 4, (int)outputfloat);
+      // SendCmdServo( 5, (int)outputfloat);
+      // SendCmdServo( 6, (int)outputfloat);
+  //    SendCmdServo( 1, 2000);
+      Serial.println((int)outputfloat);
+
+      // MAV data の要求
+      num_hbs_pasados++;
+      if(num_hbs_pasados>=num_hbs) {
+        // Pixhawk からの stream を要求する
+        Mav_Request_Data();
+        num_hbs_pasados=0;
+      }
+
+      // タイマー割り込みフラグをおろす
+      Flag_timer = 0;
     }
 
-    // タイマー割り込みフラグをおろす
-  	Flag_timer = 0;
-  }
+    // センサ値更新
+    updateSensorValues(&ctl,&roll,&pitch,&yaw,&rollspeed,&pitchspeed,&yawspeed);
 
-  // メッセージ受信関数
-  comm_receive();
+    // メッセージ受信関数
+    comm_receive();
+  }
 }
--- mavlink_with_esp32/mymavlink.ino
+++ mavlink_with_esp32/mymavlink.ino
@@ -75,10 +75,12 @@ void Mav_Request_Data() {
   const int maxStreams = 1;  // ストリームの数の設定
   // ストリームの内訳の設定
 //  const uint8_t MAVStreams[maxStreams] = { MAV_DATA_STREAM_EXTENDED_STATUS } ; //,  // extended status
-  const uint8_t MAVStreams[maxStreams] = { MAV_DATA_STREAM_RAW_SENSORS } ; //,  //
+//  const uint8_t MAVStreams[maxStreams] = { MAV_DATA_STREAM_RAW_SENSORS } ; //,  //
+  const uint8_t MAVStreams[maxStreams] = { MAV_DATA_STREAM_EXTRA1 } ; //,  //
                                            //MAV_DATA_STREAM_EXTRA1 };         // extra1
   // データストリーム毎のデータレート指定[Hz]
-  const uint16_t MAVRates[maxStreams] = { 0x01 }; //,    // 2[Hz]
+ const uint16_t MAVRates[maxStreams] = { 0x14 }; //,    // 1[Hz]
+  // const uint16_t MAVRates[maxStreams] = { 0x64 }; //,    // 100[Hz]
                                          // 0x01 };  // 5[Hz]
 
   // ストリームごとの繰り返し文
@@ -246,6 +248,315 @@ void comm_receive() {
       // char cbuf2[13+1] ;
       // sprintf(cbuf2, "[ %03d, %03d ] ", c0, c1);
       // Serial.print(cbuf2);
+
+      Serial.println(); // 改行
+      
+      break ; // while文を抜ける
+    }
+    // Serial.print(c);
+    cnt++ ; // 空の読み込み回数カウントアップ
+  }
+}
+
+// メッセージ受信関数
+void updateSensorValues(uint32_t* ctl,float* roll,float* pitch,float* yaw,float* rollspeed,float* pitchspeed,float* yawspeed) {
+  // mavlink_message_t msg;
+  // mavlink_status_t statetus;
+  // float ct, roll, pitch, yaw, rollspeed, pitchspeed, yawspeed;
+
+  int cnt = 0; // byte型の読み込み回数カウンタ
+
+  // Serial2の受信バッファにデータがあればループ
+  while( Serial2.available() > 0 ){
+
+    uint8_t c = Serial2.read(); // 1文字読み込み
+
+    // 0. 開始点（"253"）の検知
+    if( c == 253 ){
+      // デバッグ用表示
+      // Serial.println(cnt); // 読み込み数カウントの表示
+      char cbuf[4] ;
+      sprintf(cbuf, "%03d ", c);
+      Serial.print(cbuf);
+
+      // 1. len を読む
+      while( Serial2.available() < 1 ); // 受信バッファに1文字たまるまで待つ
+      uint8_t clen = Serial2.read();    // 1文字読み込み
+      sprintf(cbuf, "%03d ", clen);     // 文字列を10進整数として整形
+      Serial.print(cbuf);               // デバッグ用表示
+
+      // 2. incompatibility flag を読む
+      while( Serial2.available() < 1 );
+      c = Serial2.read();
+      sprintf(cbuf, "%03d ", c);
+      Serial.print(cbuf);
+
+      // 3. compatibility flag を読む
+      while( Serial2.available() < 1 );
+      c = Serial2.read();
+      sprintf(cbuf, "%03d ", c);
+      Serial.print(cbuf);
+
+      // 4. packet sequence number を読む
+      while( Serial2.available() < 1 );
+      c = Serial2.read();
+      sprintf(cbuf, "%03d ", c);
+      Serial.print(cbuf);
+
+      // 5. System id を読む
+      while( Serial2.available() < 1 );
+      c = Serial2.read();
+      sprintf(cbuf, "%03d ", c);
+      Serial.print(cbuf);
+
+      // 6. Component id を読む
+      while( Serial2.available() < 1 );
+      c = Serial2.read();
+      sprintf(cbuf, "%03d ", c);
+      Serial.print(cbuf);
+
+      // 7 to 9. Message id を読む
+      while( Serial2.available() < 3 );
+      uint8_t c0 = Serial2.read();
+      uint8_t c1 = Serial2.read();
+      uint8_t c2 = Serial2.read();
+      char cbuf3[18+1+11] ;
+      uint32_t ca = 0;
+      uint32_t cb = 0;
+      uint32_t cc = 0;
+      uint32_t cq = 0;
+      ca = c0;
+      cb = c1 << 8;
+      cc = c2 << 16;
+      cq = ca + cb + cc;
+      sprintf(cbuf3, "[ %03d, %03d, %03d (%08d) ] ", c0, c1, c2, cq );
+      Serial.print(cbuf3);
+
+      // if (cq==116){ 
+      uint32_t ct=0;
+
+      if (cq==30){ 
+        // uint32_t ct=0;
+        // float roll=0;
+        int32_t roll32=0;
+        int32_t rollf=0;
+        int32_t rolle=0;
+        // float pitch=0;
+        int32_t pitch32=0;
+        int32_t pitchf=0;
+        int32_t pitche=0;
+        // float yaw=0;
+        int32_t yaw32=0;
+        int32_t yawf=0;
+        int32_t yawe=0;
+        // float rollspeed=0;
+        int32_t rollspeed32=0;
+        int32_t rollspeedf=0;
+        int32_t rollspeede=0;
+        // float pitchspeed=0;
+        int32_t pitchspeed32=0;
+        int32_t pitchspeedf=0;
+        int32_t pitchspeede=0;
+        // float yawspeed=0;
+        int32_t yawspeed32=0;
+        int32_t yawspeedf=0;
+        int32_t yawspeede=0;
+   
+        if ( clen > 0 ){      // Payload の長さが0より大きいとき
+          Serial.print("[ "); // かっこはじまり
+          int c_ =0;
+        
+          for ( int i = 0; i<clen ; i++ ){  // Payload の長さ分読み込み・表示ループ
+            while( Serial2.available() < 1 );
+            c = Serial2.read();
+                  
+            if (i<4){  //4バイトを合わせて時間の数値を10進数で表す
+            ct=ct+(c<<(8*i));
+
+            }else if (i<8){  //roll計算
+              if(i==7){ //rollの負の判定
+                c_=c>>7;
+              }
+              
+            roll32=roll32+(c<<(8*(i-4))); //rolの1~4つ目の数字を合わせてrollを32bitで出す
+            
+            }else if (i<12){  //pitch計算
+              if(i==11){ //rollの負の判定
+                c_=c>>7;
+              }
+              
+            pitch32=pitch32+(c<<(8*(i-8))); //rolの1~4つ目の数字を合わせてrollを32bitで出す
+            
+            }else if (i<16){  //yaw計算
+              if(i==15){ //rollの負の判定
+                c_=c>>7;
+              }
+              
+            yaw32=yaw32+(c<<(8*(i-12))); //rolの1~4つ目の数字を合わせてrollを32bitで出す
+            
+            }else if (i<20){  //rollspeed計算
+              if(i==19){ //rollの負の判定
+                c_=c>>7;
+              }
+              
+            rollspeed32=rollspeed32+(c<<(8*(i-16))); //rolの1~4つ目の数字を合わせてrollを32bitで出す
+            
+            }else if (i<24){  //pitchspeed計算
+              if(i==23){ //rollの負の判定
+                c_=c>>7;
+              }
+              
+            pitchspeed32=pitchspeed32+(c<<(8*(i-20))); //rolの1~4つ目の数字を合わせてrollを32bitで出す
+            
+            }else if (i<28){  //yawspeed計算
+              if(i==27){ //rollの負の判定
+                c_=c>>7;
+              }
+              
+            yawspeed32=yawspeed32+(c<<(8*(i-24))); //rolの1~4つ目の数字を合わせてrollを32bitで出す
+            
+            }
+
+            if(i==4){ //時間数値（10桁）の表示
+              char cbuft[14];
+              sprintf(cbuft, "(%010d) ", ct);
+              Serial.print(cbuft);
+              *ctl=ct;
+              
+            }
+            if(i==8){//rollの表示
+              char cbufroll[54];
+
+              rollf = roll32 & (0x7FFFFF);
+              rolle = roll32 & (0x7FFFFFFF);
+              rolle = rolle>>23;
+
+              *roll = pow(-1,c_)*(1+(rollf*pow(2,-23)))*pow(2,rolle-127);
+            //   if (c_==1){
+            //   cxacc=-(65536)+cxacc; //加速度が負の場合の数値を出す
+
+            // }
+              sprintf(cbufroll, "(%.010f) ", *roll);
+            
+              Serial.print(cbufroll);
+              
+            }
+            if(i==12){//rollの表示
+              char cbufpitch[54];
+
+              pitchf = pitch32 & (0x7FFFFF);
+              pitche = pitch32 & (0x7FFFFFFF);
+              pitche = pitche>>23;
+
+              *pitch = pow(-1,c_)*(1+(pitchf*pow(2,-23)))*pow(2,pitche-127);
+            //   if (c_==1){
+            //   cxacc=-(65536)+cxacc; //加速度が負の場合の数値を出す
+
+            // }
+              sprintf(cbufpitch, "(%.010f) ", *pitch);
+            
+              Serial.print(cbufpitch);
+              
+            }
+            if(i==16){//rollの表示
+              char cbufyaw[54];
+
+              yawf = yaw32 & (0x7FFFFF);
+              yawe = yaw32 & (0x7FFFFFFF);
+              yawe = yawe>>23;
+
+              *yaw = pow(-1,c_)*(1+(yawf*pow(2,-23)))*pow(2,yawe-127);
+            //   if (c_==1){
+            //   cxacc=-(65536)+cxacc; //加速度が負の場合の数値を出す
+
+            // }
+              sprintf(cbufyaw, "(%.010f) ", *yaw);
+            
+              Serial.print(cbufyaw);
+              
+            }
+            if(i==20){//rollの表示
+              char cbufrollspeed[54];
+
+              rollspeedf = rollspeed32 & (0x7FFFFF);
+              rollspeede = rollspeed32 & (0x7FFFFFFF);
+              rollspeede = rollspeede>>23;
+
+              *rollspeed = pow(-1,c_)*(1+(rollspeedf*pow(2,-23)))*pow(2,rollspeede-127);
+            //   if (c_==1){
+            //   cxacc=-(65536)+cxacc; //加速度が負の場合の数値を出す
+
+            // }
+              sprintf(cbufrollspeed, "(%.010f) ", *rollspeed);
+            
+              Serial.print(cbufrollspeed);
+              
+            }
+            if(i==24){//rollの表示
+              char cbufpitchspeed[54];
+
+              pitchspeedf = pitchspeed32 & (0x7FFFFF);
+              pitchspeede = pitchspeed32 & (0x7FFFFFFF);
+              pitchspeede = pitchspeede>>23;
+
+              *pitchspeed = pow(-1,c_)*(1+(pitchspeedf*pow(2,-23)))*pow(2,pitchspeede-127);
+            //   if (c_==1){
+            //   cxacc=-(65536)+cxacc; //加速度が負の場合の数値を出す
+
+            // }
+              sprintf(cbufpitchspeed, "(%.010f) ", *pitchspeed);
+            
+              Serial.print(cbufpitchspeed);
+              
+            }
+            
+            sprintf(cbuf, "%03d ", c);
+            Serial.print(cbuf);
+
+            if(i==27){//rollの表示
+              char cbufyawspeed[54];
+
+              yawspeedf = yawspeed32 & (0x7FFFFF);
+              yawspeede = yawspeed32 & (0x7FFFFFFF);
+              yawspeede = yawspeede>>23;
+
+              *yawspeed = pow(-1,c_)*(1+(yawspeedf*pow(2,-23)))*pow(2,yawspeede-127);
+            //   if (c_==1){
+            //   cxacc=-(65536)+cxacc; //加速度が負の場合の数値を出す
+
+            // }
+              sprintf(cbufyawspeed, "(%.010f) ", *yawspeed);
+            
+              Serial.print(cbufyawspeed);
+              
+            }
+
+          }
+          
+          Serial.print("] "); // かっこ終わり
+        }
+      }
+      else {
+        // 9+clen. Payload を読む
+        if ( clen > 0 ){      // Payload の長さが0より大きいとき
+          Serial.print("[ "); // かっこはじまり
+          for ( int i = 0; i<clen ; i++ ){  // Payload の長さ分読み込み・表示ループ
+            while( Serial2.available() < 1 );
+            c = Serial2.read();
+            sprintf(cbuf, "%03d ", c);
+            Serial.print(cbuf);
+          }
+          Serial.print("] "); // かっこ終わり
+        }
+      }
+
+      // 10+clen to 11+clen. checksum を読む
+      while( Serial2.available() < 2 );
+      c0 = Serial2.read();
+      c1 = Serial2.read();
+      char cbuf2[13+1] ;
+      sprintf(cbuf2, "[ %03d, %03d ] ", c0, c1);
+      Serial.print(cbuf2);
 
       Serial.println(); // 改行
       
